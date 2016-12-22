ff07cda9b3bcd626dc363beb0c2266e5a033b030
Akhan Ismailov <404akhan@gmail.com>
2016-12-22 16:15:30 +0800

    default

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello world, second.');
+  res.send('hello world, third.');
 });
  
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000, function({
+	console.log('LISTENING PORT 5000');
+});
  
 module.exports = app;
 
